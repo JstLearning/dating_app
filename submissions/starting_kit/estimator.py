26dfce61402e0f2b5355bc581a82f0e1c1fb075b
agatheminaro <agathe.minaro@ensae.fr>
2023-02-23 11:04:11 +0100

    add starting kit estimator

---
+++ submissions/starting_kit/estimator.py
@@ -0,0 +1,41 @@
+from sklearn.base import BaseEstimator
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.impute import SimpleImputer
+
+
+class Regressor(BaseEstimator):
+    def __init__(self):
+        self.model = RandomForestRegressor()
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        y_pred = self.model.predict(X)
+        return y_pred
+
+
+class FeatureExtractor(BaseEstimator):
+    def fit(self, X, y):
+        self.nb_users_by_age = X.groupby("age").agg({"gender": "count"}).reset_index()
+        self.nb_users_by_age.columns = ["age", "nb_users"]
+        return self
+
+    def transform(self, X):
+        return X.merge(self.nb_users_by_age, on="age", how="left")
+
+
+def get_estimator():
+
+    feature_extractor = FeatureExtractor()
+
+    reg = Regressor()
+
+    impute_missing_values = SimpleImputer(strategy="mean")
+
+    pipe = make_pipeline(
+        feature_extractor, impute_missing_values, StandardScaler(), reg
+    )
+    return pipe
