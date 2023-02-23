b3f4c70fc0ea3bc8078387fa7c1b825b2185f392
agatheminaro <agathe.minaro@ensae.fr>
2023-02-23 11:29:08 +0100

    add workflow

--- problem.py
+++ problem.py
@@ -50,7 +50,19 @@ def get_data(test_size=0.2, random_state=42):
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size, random_state=random_state
     )
-    return X_train, X_test, y_train, y_test
+    return X_train, X_test, y_train["counts_kisses"], y_test["counts_kisses"]
+
+
+def get_train_data():
+    """Return the training data as a pandas DataFrame"""
+    X_train, _, y_train, _ = get_data()
+    return X_train, y_train
+
+
+def get_test_data():
+    """Return the test data as a pandas DataFrame"""
+    _, X_test, _, y_test = get_data()
+    return X_test, y_test
 
 
 ######################
